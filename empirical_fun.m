70722419ff99dbec53af765ecf6086f49a1a6981
jormig01 <99514557+jormig01@users.noreply.github.com>
2022-03-07 11:27:32 +0100

    UY UY UY semicolumn

--- empirical_fun.m
+++ empirical_fun.m
@@ -4,6 +4,6 @@ function [As Sk] = empirical_fun(Ur)
 B = 0.857/(1+exp((-0.471-log10(Ur))/(0.297)));
 psi = -90+90*tanh(0.815/(Ur.^0.672)); %It will give the result in degrees
 
-As = B*sind(psi)
-Sk = B*cosd(psi)
+As = B*sind(psi);
+Sk = B*cosd(psi);
 end
